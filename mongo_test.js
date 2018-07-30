779937d90982f8fcd399d5caa0d23190c39340c9
Tim <tim.huang61@gmail.com>
2018-07-30 12:02:15 +0800

    add how to connect mongodb sample

--- mongo_test.js
+++ mongo_test.js
@@ -5,10 +5,38 @@ const mongoRouter = express.Router();
 const url = 'mongodb://localhost:27017';
 const dbName = 'sample';
 
-mongoRouter.get('/', (req, resp) => {
-    MongoClient.connect(url, function(error, client){
-        const db = client.db('sample');
-        db.collection('students').find({name:/Tim/}).toArray((error, docs) => resp.json(docs))
+// Query
+mongoRouter.get('/query', (req, resp) => {
+    MongoClient.connect(url, function (error, client) {
+        const db = client.db(dbName);
+        db.collection('students').find({
+            name: /Tim/
+        }).toArray((error, docs) => resp.json(docs));
+        client.close();
+    });
+});
+
+//Insert 
+mongoRouter.get('/insert', (req, resp) => {
+    MongoClient.connect(url, function (error, client) {
+        const db = client.db(dbName);
+        db.collection('students').insert({
+            name: 'Wendy',
+            telephone: '(06)22222222'
+        }, (error, result) => resp.json('Ok'));
+        client.close();
+    });
+});
+
+//Update 
+mongoRouter.get('/update', (req, resp) => {
+    MongoClient.connect(url, function (error, client) {
+        const db = client.db(dbName);
+        db.collection('students').update({
+            name: 'Tim'
+        }, {
+            telephone: '(06)22222222'
+        }, (error, result) => resp.json('Ok'));
         client.close();
     });
 });
